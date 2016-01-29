a075307441baeac7a21e293349fd0528c4d11aab
Steven Leiva <stevenleiva@Stevens-MacBook-Pro.local>
2016-01-28 20:47:03 -0500

    Change the es5 project to use the properties on the Match component.

--- es5/js/main.js
+++ es5/js/main.js
@@ -6,7 +6,7 @@ var MatchesBox = React.createClass({
       <div className="matches-box">
         <h1>Here Are Your Matches</h1>
         <MatchesList />
-        <Match />
+        <Match position="President" employer="U.S.A." salary="$400k" bonus="Politcal Graft Only" />
       </div>
     );
   }
@@ -24,10 +24,10 @@ var Match = React.createClass({
   render: function() {
     return (
       <div className="match">
-        <p><strong>Position:</strong> President</p>
-        <p><strong>Employer:</strong> U.S.A. </p>
-        <p><strong>Salary Range:</strong> $400k (Non-negotionable)</p>
-        <p><strong>Eligible For Bonus:</strong> Political Graft Only</p>
+        <p><strong>Position:</strong> {this.props.position}</p>
+        <p><strong>Employer:</strong> {this.props.employer} </p>
+        <p><strong>Salary Range:</strong> {this.props.salary} (Non-negotionable)</p>
+        <p><strong>Eligible For Bonus:</strong> {this.props.bonus}</p>
       </div>
     );
   }
